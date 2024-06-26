9a0fbe3170c28584861047dc9c001e710fdfac2f
thenumbernine <christopher.e.moore@gmail.com>
2024-06-26 15:03:02 +0600

    added run-file and interpreter support

--- run.lua
+++ run.lua
@@ -2,7 +2,8 @@
 require 'ext'
 --require 'local-default' -- ... but this uses a shim parser, which langfix does too, so hmm
 if jit then require 'ext.ctypes' end
-require 'langfix'
+--require 'langfix'
+require 'langfix.env'(_G)
 
 -- and while we're here , modify package.path to accept our new extension as well
 -- or should this be done in langfix.lua?
@@ -23,9 +24,44 @@ package.path = parts:concat';'
 local oldarg = arg
 arg = {[0]=oldarg[1], table.unpack(arg, 2)}
 
-local fin = assert(..., 'expected filename')
-if path(fin):exists() then
-	assert(loadfile(fin))(select(2, ...))
+-- TODO here handle all flags, stop at -- or filename
+
+local fn
+do
+	local i = 1
+	while i <= #arg do
+		local s = arg[i]
+		-- -e
+		if s:sub(1,2) == '-e' then
+			-- -ecode
+			if #s > 2 then
+				assert(load(s:sub(3)))(select(2, ...))
+				table.remove(arg, i)
+				i = i - 1
+			else
+			-- -e code
+				if i == #arg then
+					-- print help
+				else
+					assert(load(arg[i+1]))(select(2, ...))
+					table.remove(arg, i)
+					table.remove(arg, i)
+					i = i - 1
+				end
+			end
+		else
+			-- if it's a flag then handle it
+			-- otherwise stop and this is our filename
+			fn = s
+		end
+		i = i + 1
+	end
+end
+if not fn then
+	-- interpretive mode here
+	require 'interpreter'(_G)
+elseif not path(fn):exists() then
+	io.stderr:write('lua: cannot open '..fn..': No such file or directory')
 else
-	assert(load(fin))(select(2, ...))
+	assert(loadfile(fin))(select(2, ...))
 end
